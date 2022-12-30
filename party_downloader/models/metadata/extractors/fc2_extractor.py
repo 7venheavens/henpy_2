d39ab51e0e171a049f4b300102e16179312a9289
7ven <7venheavens@gmail.com>
2022-12-30 12:42:27 +0800

    feat: fc2 extractor

---
+++ party_downloader/models/metadata/extractors/fc2_extractor.py
@@ -0,0 +1,101 @@
+from __future__ import annotations
+from party_downloader.models.metadata.extractors.base_metadata_extractor import (
+    BaseMetadataExtractor,
+)
+from party_downloader.models.web_data import WebData
+from functools import cached_property
+from datetime import date
+import re
+
+
+class FC2Extractor(BaseMetadataExtractor):
+    def __init__(self, webdata: WebData):
+        super().__init__(webdata)
+        # Soup derivatives for ease of use
+        self._container = webdata.soup
+
+    @cached_property
+    def id(self) -> str:
+        return (
+            "FC2-PPV" + self._container.find("title").text.split("FC2-PPV")[1].strip()
+        )
+
+    @cached_property
+    def title(self) -> str:
+        return self._container.find("title").text.split("FC2-PPV")[0].strip()
+
+    @property
+    def producer(self) -> str:
+        return (
+            self._container.find(class_="items_comment_sellerBox")
+            .find("h4")
+            .find("a")
+            .text
+        )
+
+    @property
+    def publisher(self) -> str:
+        return ""
+
+    @property
+    def studio(self) -> str:
+        return ""
+
+    @property
+    def series(self) -> str:
+        return ""
+
+    @property
+    def actors(self) -> list:
+        return []
+
+    @property
+    def tags(self) -> list[tuple[str, str]]:
+        """Returns a list of tuples of (tag_id, tag_name)
+        Tag name can vary across time as this often gets updated
+        """
+        tag_data = self._container.find_all("a", class_="tag")
+        tags = [
+            (tag["href"].split("/")[-1], tag.text.strip()) for tag in tag_data if tag
+        ]
+        return [
+            (tag[0][5:], tag[1]) if tag[0].startswith("?tag=") else tag for tag in tags
+        ]
+
+    @property
+    def release_date(self) -> date | None:
+        raw_date_text = self._container.find(class_="items_article_Releasedate")
+        match = re.search(r"(\d+/\d+/\d+)", raw_date_text.text)
+        if not match:
+            return None
+        raw_date = match.group(1)
+        return date.fromisoformat(raw_date.replace("/", "-"))
+
+    @property
+    def duration(self) -> int | None:
+        ele = self._container.find(class_="items_article_info")
+        match = re.search(r"(\d+:\d+:\d+)", ele.text)
+        if not match:
+            return 0
+        mult = [3600, 60, 1]
+        time = match.group(1).split(":")
+        return sum([int(t) * m for t, m in zip(time, mult)])
+
+    @property
+    def cover_url(self) -> str:
+        cover = self._container.find("div", class_="items_article_MainitemThumb").find(
+            "img"
+        )
+        href = cover["src"]
+        if not href:
+            return ""
+        return self.webdata.clean_url_link(href)
+
+    @property
+    def thumbnail_urls(self) -> list[str]:
+        thumbs = self._container.find(class_="items_article_SampleImagesArea").find_all(
+            "a"
+        )
+        if not thumbs:
+            return []
+        return [self.webdata.clean_url_link(thumb["href"]) for thumb in thumbs]
---
+++ test/test_fc2_extractor.py
@@ -0,0 +1,74 @@
+from party_downloader.models.metadata.extractors.fc2_extractor import FC2Extractor
+from party_downloader.models.web_data import WebData
+from datetime import date
+import pytest
+from pathlib import Path
+
+
+@pytest.fixture
+def extractor(request):
+    with open(
+        Path(__file__).parent
+        / Path(f"data/metadata_scrapers/fc2/{request.param}.html"),
+        encoding="utf-8",
+    ) as f:
+        page_data = f.read()
+    page = WebData(
+        f"https://adult.contents.fc2.com/article/{request.param}", page_data=page_data
+    )
+    extractor = FC2Extractor(page)
+    return extractor
+
+
+correct_metadata = {
+    "3104128": {
+        "id": "FC2-PPV-3104128",
+        "title": "正真正銘初撮りの原石！地味巨乳JD娘に生中出し。こんな子とできるなんて。",
+        "description": "",
+        "producer": "ギガ珍太郎",
+        "publisher": "",
+        "studio": "",
+        "series": "",
+        "actors": [],
+        "tags": [
+            ("%E3%83%8F%E3%83%A1%E6%92%AE%E3%82%8A", "ハメ撮り"),
+            ("%E7%B4%A0%E4%BA%BA", "素人"),
+            ("%E7%BE%8E%E4%B9%B3", "美乳"),
+            ("%E4%B8%AD%E5%87%BA%E3%81%97", "中出し"),
+            ("%E5%80%8B%E4%BA%BA%E6%92%AE%E5%BD%B1", "個人撮影"),
+            ("%E3%83%95%E3%82%A7%E3%83%A9", "フェラ"),
+            ("%E3%82%AA%E3%83%AA%E3%82%B8%E3%83%8A%E3%83%AB", "オリジナル"),
+            ("%E3%81%8B%E3%82%8F%E3%81%84%E3%81%84", "かわいい"),
+            ("%E5%A5%B3%E5%AD%90%E5%A4%A7%E7%94%9F", "女子大生"),
+        ],
+        "release_date": date(2022, 10, 3),
+        "duration": 4355,
+        "cover_url": "https://contents-thumbnail2.fc2.com/w276/storage71000.contents.fc2.com/file/367/36679264/1664518411.89.jpg",
+        "thumbnail_urls": [
+            "https://storage71000.contents.fc2.com/file/367/36679264/1664518411.96.jpg",
+            "https://storage71000.contents.fc2.com/file/367/36679264/1664518412.23.jpg",
+            "https://storage71000.contents.fc2.com/file/367/36679264/1664518412.5.jpg",
+            "https://storage71000.contents.fc2.com/file/367/36679264/1664518412.78.jpg",
+            "https://storage71000.contents.fc2.com/file/367/36679264/1664518413.03.jpg",
+            "https://storage71000.contents.fc2.com/file/367/36679264/1664518413.3.jpg",
+            "https://storage71000.contents.fc2.com/file/367/36679264/1664518413.57.jpg",
+            "https://storage71000.contents.fc2.com/file/367/36679264/1664518413.83.jpg",
+            "https://storage70000.contents.fc2.com/file/367/36679264/1664518677.96.jpg",
+        ],
+    },
+}
+
+
+@pytest.mark.parametrize(
+    "extractor, expected_metadata",
+    [
+        (extractor, expected_metadata)
+        for extractor, expected_metadata in correct_metadata.items()
+    ],
+    indirect=["extractor"],
+)
+def test_extractor(extractor, expected_metadata):
+    metadata = extractor.metadata
+    # Allow for partials as whole might be too time consuming to handle
+    for key, val in expected_metadata.items():
+        assert metadata[key] == val
