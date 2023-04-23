976821d69c727aeba651c12da5dc47acd2ba1855
7ven <7venheavens@gmail.com>
2023-04-23 21:08:28 +0800

    feat: expansion of filenames and unicode fixes

--- party_downloader/models/download_metadatum.py
+++ party_downloader/models/download_metadatum.py
@@ -1,15 +1,20 @@
 from dataclasses import dataclass
-from urllib.parse import urlparse
+from urllib.parse import urlparse, unquote
 
 
 @dataclass
 class DownloadMetadatum:
+    """Dataclass that contains the full set of information necessary to performa download"""
+
     path: str
     filetype: str
     key: tuple
     filename: str = None
+    raw_filename: str = None
 
     def __post_init__(self):
         parsed = urlparse(self.path)
         _, fn = parsed.query.split("=")
-        self.filename = fn
+        self.raw_filename = fn
+        # Convert from %C3 to proper characters
+        self.filename = unquote(fn)
