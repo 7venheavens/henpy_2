8c25962134bbd48560470b206af484609214b56f
7ven <7venheavens@gmail.com>
2023-04-25 01:23:03 +0800

    fix: Added is_multiple and age verification checks

--- party_downloader/metadata/scrapers/msin_scraper.py
+++ party_downloader/metadata/scrapers/msin_scraper.py
@@ -4,11 +4,16 @@ from party_downloader.metadata.scrapers.base_metadata_scraper import BaseMetadat
 from party_downloader.models.web_data import WebData
 from pathlib import Path
 from party_downloader.helpers import Regexes
+import requests
 
 
 class MSINScraper(BaseMetadataScraper):
     COMPONENT_REGEX = Regexes.FC2
-    SEARCH_TEMPLATE = "https://db.msin.jp/branch/search?sort=movie&str={id_num}"
+    SEARCH_TEMPLATE = "https://db.msin.jp/branch/search?sort=movie&str={}"
+
+    def __init__(self):
+        super().__init__()
+        self.session.cookies.set("age", "off")
 
     @classmethod
     def get_id_components(cls, file: str | Path) -> tuple[str, str] | None:
@@ -17,6 +22,24 @@ class MSINScraper(BaseMetadataScraper):
             res["name"] = f'fc2-ppv-{res["id"]}'
         return res
 
-    def is_valid(self, webdata: WebData):
-        if webdata.soup.find(class_="error_string"):
+    @staticmethod
+    def is_multiple(webdata: WebData):
+        if webdata.soup.find(class_="movie_view"):
+            return True
+        return False
+
+    @staticmethod
+    def is_valid(webdata: WebData):
+        error = webdata.soup.find(class_="error_string")
+        if error and error.text == "No Results":
             raise ValueError("No results found")
+
+        if MSINScraper.is_multiple(webdata):
+            raise ValueError("Multiple results found")
+        MSINScraper.is_age_verified(webdata)
+
+    @staticmethod
+    def is_age_verified(webdata: WebData):
+        target = webdata.soup.find(class_="error_massage")
+        if target and target.text == "年齢確認":
+            raise ValueError("Age verification required")
