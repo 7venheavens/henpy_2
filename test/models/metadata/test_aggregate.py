b3c442fdc43c3280420a2dc91022f9f7635cfbfc
7ven <7venheavens@gmail.com>
2023-06-15 20:57:01 +0800

    fix: working

--- test/models/metadata/test_aggregate.py
+++ test/models/metadata/test_aggregate.py
@@ -12,12 +12,17 @@ def test_aggregation(tmp_path):
     # Split files
     (dir / "fc2-ppv-1231233_1.mp4").touch()
     (dir / "fc2-ppv-1231233_2.mp4").touch()
+    (dir / "FC2PPV-1717523 2.mp4").touch()
+    (dir / "FC2-PPV-1717523 1.mp4").touch()
+
     # Invalid files
     (dir / "sdaflkasdlkfas.mp4").touch()
-    assert len(os.listdir(dir)) == 4
+    assert len(os.listdir(dir)) == 6
 
     batched, unprocessed = aggregate_files(dir, FC2Scraper)
     assert len(batched["fc2-ppv-1231232"]) == 1
     assert len(batched["fc2-ppv-1231233"]) == 2
+    assert len(batched["fc2-ppv-1717523"]) == 2
+    raise Exception(batched)
     assert len(unprocessed) == 1
     assert unprocessed[0] == "sdaflkasdlkfas.mp4"
