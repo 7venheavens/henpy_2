92d43510985ee9bffa91a70cc7fa59d564809814
7ven <7venheavens@gmail.com>
2023-06-27 21:32:52 +0800

    feat: jal scraper

---
+++ party_downloader/metadata/scrapers/javlibrary_scraper.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+from party_downloader.metadata.scrapers.base_metadata_scraper import BaseMetadataScraper
+from pathlib import Path
+from party_downloader.models.web_data import WebData
+from party_downloader.helpers import Regexes
+
+
+class JAVLibraryScraper(BaseMetadataScraper):
+    COMPONENT_REGEX = Regexes.JAV
+    SEARCH_TEMPLATE = "https://www.javlibrary.com/en/vl_searchbyid.php?keyword={id}"
+
+    @classmethod
+    def get_id_components(cls, file: str | Path) -> tuple[str, str] | None:
+        file = Path(file)
+        groups = cls.COMPONENT_REGEX.findall(file.stem)
+        if len(groups) != 1:
+            raise Exception(f"Unable to extract ids: {file.stem}")
+
+        id = f"{groups[0][0].upper()}-{groups[0][1]}"
+
+        res = {"id": id, "part": None, "name": id}
+        if res["part"]:
+            res["part"] = int(res["part"])
+        return res
+
+    @staticmethod
+    def is_valid(webdata: WebData):
+        if webdata.soup.find(text="Search returned no result."):
+            raise ValueError("Invalid data")
+
+    @staticmethod
+    def is_multiple(webdata: WebData):
+        videos = webdata.soup.find_all(class_="video")
+        return len(videos) > 1
+
+    def search(self, query: str) -> WebData:
+        res = WebData(self.SEARCH_TEMPLATE.format(id=query))
+        self.is_valid(res)
+        return res
